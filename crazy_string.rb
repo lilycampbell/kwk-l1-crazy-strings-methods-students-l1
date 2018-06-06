c4608bec459575414132272842cfb33a177cdb25
lilycampbell <lilycatcampbell@gmail.com>
2018-06-06 20:07:52 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,9 +1,9 @@
 # write your method here
 
 def crazy_strings(a,b)
-a = {uppercase, reversed} 
-b = {swapcase, replace 's' with 'z'}
-puts (Hello, Friends)
+  a = {uppercase, reversed} 
+  b = {swapcase, replace 's' with 'z'}
+ puts (Hello, Friends)
 end
 
 crazy_strings(a,b)
